b742d34fa618f25d716cd446664b55d9d6e1a9fb
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-11-11 01:24:31 +1300

    libvorbis-1.3.3

---
+++ infusion.rb
@@ -0,0 +1,34 @@
+
+#
+#  This file is part of the "Teapot" project, and is released under the MIT license.
+#
+
+required_version "0.1"
+
+define_package "libvorbis-1.3.3" do |package|
+	package.depends = ["libogg"]
+
+	package.build(:all) do |platform, environment|
+		environment.use in: package.source_path do |config|
+			Dir.chdir(package.source_path) do
+				Commands.run("make", "clean") if File.exist? "Makefile"
+				
+				# The default configure has problems when compiling with multiple architectures because of the following option..
+				# so we remove it forcefully.
+				Commands.run("sed", "-i", "-e", "s/-force_cpusubtype_ALL//g", "configure", "configure.ac")
+				
+				Commands.run("./configure",
+					"--prefix=#{platform.prefix}",
+					"--disable-dependency-tracking",
+					"--enable-shared=no",
+					"--enable-static=yes", 
+					*config.configure
+				)
+				
+				Commands.run("make install")
+			end
+		end
+		
+		Commands.run("cp", "FindVorbis.cmake", platform.cmake_modules_path.to_s)
+	end
+end
