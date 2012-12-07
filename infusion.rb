b09e8728c3538b099aa77908941ca12da42dc018
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-12-07 14:38:31 +1300

    Update to new dependency based build.

--- infusion.rb
+++ infusion.rb
@@ -3,30 +3,32 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-required_version "0.1"
+required_version "0.5"
 
-define_package "libvorbis-1.3.3" do |package|
-	package.depends = ["libogg"]
-
-	package.build(:all) do |platform, environment|
-		environment.use in: package.source_path do |config|
-			Dir.chdir(package.source_path) do
-				Commands.run("make", "clean") if File.exist? "Makefile"
+define_package "libvorbis" do |package|
+	package.install do |environment|
+		environment.use in:(package.path + 'libvorbis-1.3.3') do |config|
+			Commands.run("make", "clean") if File.exist? "Makefile"
 				
-				# The default configure has problems when compiling with multiple architectures because of the following option..
-				# so we remove it forcefully.
-				Commands.run("sed", "-i", "-e", "s/-force_cpusubtype_ALL//g", "configure", "configure.ac")
+			# The default configure has problems when compiling with multiple architectures because of the following option.. so we remove it forcefully :D
+			Commands.run("sed", "-i", "-e", "s/-force_cpusubtype_ALL//g", "configure", "configure.ac")
 				
-				Commands.run("./configure",
-					"--prefix=#{platform.prefix}",
-					"--disable-dependency-tracking",
-					"--enable-shared=no",
-					"--enable-static=yes", 
-					*config.configure
-				)
+			Commands.run("./configure",
+				"--prefix=#{config.install_prefix}",
+				"--disable-dependency-tracking",
+				"--enable-shared=no",
+				"--enable-static=yes", 
+				*config.configure
+			)
 				
-				Commands.run("make install")
-			end
+			Commands.run("make", "install")
 		end
+	end
+	
+	package.depends :system
+	package.depends "Library/ogg"
+	
+	package.provides "Library/vorbis" do
+		append linkflags ["-lvorbis", "-lvorbisfile"]
 	end
 end
