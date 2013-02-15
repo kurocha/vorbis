15532f69592e9ba7d6c1e3175841c407295dc923
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2013-02-15 13:38:26 +1300

    Update to support teapot 0.6.

--- teapot.rb
+++ teapot.rb
@@ -3,25 +3,24 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-required_version "0.5"
+required_version "0.6"
 
 define_target "vorbis" do |target|
 	target.install do |environment|
-		environment.use in:(package.path + 'libvorbis-1.3.3') do |config|
-			Commands.run("make", "clean") if File.exist? "Makefile"
+		install_external(package.path, "libvorbis-1.3.3", environment) do |config, fresh|
+			if fresh
+				Commands.run("sed", "-i", "-e", "s/-force_cpusubtype_ALL//g", "configure", "configure.ac")
 				
-			# The default configure has problems when compiling with multiple architectures because of the following option.. so we remove it forcefully :D
-			Commands.run("sed", "-i", "-e", "s/-force_cpusubtype_ALL//g", "configure", "configure.ac")
-				
-			Commands.run("./configure",
-				"--prefix=#{config.install_prefix}",
-				"--disable-dependency-tracking",
-				"--enable-shared=no",
-				"--enable-static=yes", 
-				*config.configure
-			)
-				
-			Commands.run("make", "install")
+				Commands.run("./configure",
+					"--prefix=#{config.install_prefix}",
+					"--disable-dependency-tracking",
+					"--enable-shared=no",
+					"--enable-static=yes", 
+					*config.configure
+				)
+			end
+			
+			Commands.make_install
 		end
 	end
 	
